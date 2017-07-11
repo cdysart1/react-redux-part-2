c6c350479d00ea67effd5bccc68013d9396450d6
Carol Dysart <dysart.carol@gmail.com>
2017-07-11 11:17:07 -0400

    channels

--- starting-point/client/store.js
+++ starting-point/client/store.js
@@ -10,7 +10,8 @@ import socket from './socket';
 const initialState = {
   messages: [],
   name: 'Reggie',
-  newMessageEntry: ''
+  newMessageEntry: '',
+  channels: []
 };
 
 // ACTION TYPES
@@ -19,34 +20,40 @@ const UPDATE_NAME = 'UPDATE_NAME';
 const GET_MESSAGE = 'GET_MESSAGE';
 const GET_MESSAGES = 'GET_MESSAGES';
 const WRITE_MESSAGE = 'WRITE_MESSAGE';
+const GET_CHANNELS = 'GET_CHANNELS';
 
 // ACTION CREATORS
 
-export function updateName (name) {
+export function updateName(name) {
   const action = { type: UPDATE_NAME, name };
   return action;
 }
 
-export function getMessage (message) {
+export function getMessage(message) {
   const action = { type: GET_MESSAGE, message };
   return action;
 }
 
-export function getMessages (messages) {
+export function getMessages(messages) {
   const action = { type: GET_MESSAGES, messages };
   return action;
 }
 
-export function writeMessage (content) {
+export function writeMessage(content) {
   const action = { type: WRITE_MESSAGE, content };
+  return action;
+}
+
+export function getChannels(channels) {
+  const action = { type: GET_CHANNELS, channels:channels };
   return action;
 }
 
 // THUNK CREATORS
 
-export function fetchMessages () {
+export function fetchMessages() {
 
-  return function thunk (dispatch) {
+  return function thunk(dispatch) {
     return axios.get('/api/messages')
       .then(res => res.data)
       .then(messages => {
@@ -56,9 +63,9 @@ export function fetchMessages () {
   }
 }
 
-export function postMessage (message) {
+export function postMessage(message) {
 
-  return function thunk (dispatch) {
+  return function thunk(dispatch) {
     return axios.post('/api/messages', message)
       .then(res => res.data)
       .then(newMessage => {
@@ -68,6 +75,14 @@ export function postMessage (message) {
       });
   }
 
+}
+
+export function fetchChannels () {
+  return function (dispatch) {
+    axios.get('/api/channels')
+      .then(res => res.data)
+      .then(channels => dispatch(getChannels()));
+  }
 }
 
 // REDUCER
@@ -94,7 +109,7 @@ export function postMessage (message) {
  * Note: this is still an experimental language feature (though it is on its way to becoming official).
  * We can use it now because we are using a special babel plugin with webpack (babel-preset-stage-2)!
  */
-function reducer (state = initialState, action) {
+function reducer(state = initialState, action) {
 
   switch (action.type) {
 
@@ -121,6 +136,8 @@ function reducer (state = initialState, action) {
         ...state,
         newMessageEntry: action.content
       };
+    case GET_CHANNELS:
+      return {...state, channels: action.channels };
 
     default:
       return state;
